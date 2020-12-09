44f0313b4c075b8904fb38567e87367daddd9bb8
Mr. D <analystdeepakbhatt@gmail.com>
2020-12-09 16:36:44 +0000

    msg

---
+++ Getting_and_knowing_your_data.py
@@ -0,0 +1,8 @@
+#GETTING AND KNOWING YOUR DATA :
+
+#Step 1. Import the necessary libraries
+import pandas as pd
+import numpy as np
+
+chipo=pd.read_csv('../datafile/data_chipotle.tsv')
+print(chipo.head(10))
